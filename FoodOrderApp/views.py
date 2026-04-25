ef4038a6cc5e5e7a23d972bd5bb5b4966d5d9242
Aung Phone Myat <aungphonemyat.usa@gmail.com>
2026-04-24 22:53:25 -0400

    testing food model

--- FoodOrderApp/views.py
+++ FoodOrderApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import request,HttpResponse
-
+from .models import Food
 # Create your views here.
 def index(reqeust):
-    return HttpResponse("Hello I am index")
+    foods = Food.objects.all()
+    print(foods)
+    context = {"foods":foods}
+    return render(request,"index.html",context)
